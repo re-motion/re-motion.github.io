8c48afc2512ec1fc25eb02a4a474186cc898e056
Michael Ketting <michael.ketting@rubicon.eu>
2022-07-25 16:37:17 +0200

    Create basic solution with Statiq references.

---
+++ src/SiteGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: 2022 RUBICON IT GmbH, www.rubicon.eu
+
+using System;
+
+namespace SiteGenerator;
+
+public class Program
+{
+  public static async Task<int> Main (string[] args) =>
+      await Bootstrapper
+          .Factory
+          .CreateDefault (args)
+          .RunAsync();
+}
